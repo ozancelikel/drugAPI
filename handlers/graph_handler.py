e573c3d3f1ffdb22bc27967a76f8c254ddaa06c7
ozancelikel <ozansmailcelikel@gmail.com>
2024-05-20 20:04:54 +0300

    graph added to /recursive_autocomplete

--- handlers/graph_handler.py
+++ handlers/graph_handler.py
@@ -6,29 +6,24 @@ class GraphHandler:
     def __init__(self, uri, auth):
         self.__graph = Graph(f"{uri}", auth=auth)
 
-    def add_new_relation(self, node_1_lbl: str, node_2_lbl: str):
+    def add_new_relation(self, node_1_lbl: str, node_2_lbl: str, level: str):
         node_1 = Node("Atc", name=node_1_lbl)
         node_2 = Node("Atc", name=node_2_lbl)
         try:
-            self.__graph.create(Relationship(node_1, "INTERACTS", node_2, level="SEVERE"))
-            self.__graph.create(Relationship(node_2, "INTERACTS", node_1, level="SEVERE"))
+            self.__graph.create(Relationship(node_1, "INTERACTS", node_2, level=level))
+            self.__graph.create(Relationship(node_2, "INTERACTS", node_1, level=level))
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
 
     def get_relationship(self, atc_1, atc_2):
         node_1 = self.get_node(atc_1)
         node_2 = self.get_node(atc_2)
-
+        if node_1 is None or node_2 is None:
+            return None
         res = self.__graph.match((node_1, node_2)).first()
-        if res:
-            return res
-        else:
-            raise HTTPException(status_code=500, detail=f"Relationship {atc_1} - {atc_2} not found.")
+        return res
 
     def get_node(self, node_name):
         res = self.__graph.nodes.match("Atc", name=node_name).first()
-        if res:
-            return res
-        else:
-            raise HTTPException(status_code=500, detail=f"Node {node_name} not found.")
-
+        return res
+       # raise HTTPException(status_code=500, detail=f"Node {node_name} not found.")
--- main.py
+++ main.py
@@ -6,13 +6,16 @@ import uvicorn
 
 from handlers.es_aggregator import EsAggregator
 from handlers.interaction_api import InteractionAPI
+from handlers.graph_handler import GraphHandler
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-es_agg = EsAggregator("drugs", 9200)
+es_agg = EsAggregator("drugs", 9201)
 interaction_api = InteractionAPI("localhost", 5163)
-
+URI = "neo4j+s://e7958ee4.databases.neo4j.io:7687"
+AUTH = ("neo4j", "PefqeKG_6KZPCGLC-VYFuEH4RTr_HzgDX6aNWPnKQa8")
+graph_handler = GraphHandler(URI, AUTH)
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
@@ -36,8 +39,14 @@ async def search_endpoint(data: str = Form(...), stored: str = Form(...)):
     interaction_list = []
     stored_atc = es_agg.get_atc_from_brand(stored)
     for key, val in autocomplete_list.items():
-        interaction = interaction_api.get_interactions(val, stored_atc)
-        interaction_list.append({"drug": key, "atc": val, "interaction_level": interaction})
+        interaction = graph_handler.get_relationship(val, stored_atc)
+        if interaction is not None:
+            interaction_list.append({"drug": key, "atc": val, "interaction_level": interaction})
+        else:
+            interaction = interaction_api.get_interactions(val, stored_atc)
+            interaction_list.append({"drug": key, "atc": val, "interaction_level": interaction})
+            # TODO: Check if interaction level is parsed correctly
+            graph_handler.add_new_relation(val, stored_atc, interaction)
     return interaction_list
 
 
