1e5ac30750d1975db83b7e6edcb75c6e925668e5
ozancelikel <ozansmailcelikel@gmail.com>
2024-05-21 00:56:00 +0300

    update graph_handler.py added Driver support, added neo4j compose

--- handlers/graph_handler.py
+++ handlers/graph_handler.py
@@ -1,33 +1,73 @@
-from fastapi import HTTPException
-from py2neo import Graph, Node, Relationship
+from neo4j import GraphDatabase
 
 
 class GraphHandler:
-    def __init__(self, uri: str, auth: tuple):
-        self.__graph = Graph(f"{uri}", auth=auth)
 
-    def add_new_relation(self, node_1_lbl: str, node_2_lbl: str, level: dict):
-        node_1 = self.get_node(node_1_lbl)
-        node_2 = self.get_node(node_2_lbl)
-        if node_1 is None:
-            node_1 = Node("Atc", name=node_1_lbl)
-        if node_2 is None:
-            node_2 = Node("Atc", name=node_2_lbl)
-        try:
-            self.__graph.create(Relationship(node_1, "INTERACTS", node_2, level=level[0]["interactionLevel"]))
-            self.__graph.create(Relationship(node_2, "INTERACTS", node_1, level=level[0]["interactionLevel"]))
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
-    def get_relationship(self, atc_1, atc_2):
-        node_1 = self.get_node(atc_1)
-        node_2 = self.get_node(atc_2)
-        if node_1 is None or node_2 is None:
+    def close(self):
+        self.driver.close()
+
+    def read_node(self, node_name: str):
+        with self.driver.session() as session:
+            res = session.execute_read(self.select_node, node_name)
+            return res
+
+    def write_node(self, node_name: str):
+        with self.driver.session() as session:
+            session.execute_write(self.add_atc, node_name)
+
+    def write_relation(self, node_1: str, node_2: str, level: str):
+        self.write_node(node_1)
+        self.write_node(node_2)
+        with self.driver.session() as session:
+            session.execute_write(self.create_interaction,
+                                  node_1, node_2, level)
+
+    def read_relation(self, node_1: str, node_2: str):
+        with self.driver.session() as session:
+            res = session.execute_read(self.select_interaction,
+                                       node_1, node_2)
+            return res
+
+    @classmethod
+    def select_node(cls, tx, node_name):
+        res = tx.run("MATCH (n:Atc) "
+                     "WHERE n.name = $node_name "
+                     "RETURN n", node_name=node_name)
+        if res.peek() is None:
             return None
-        res = self.__graph.match((node_1, node_2)).first()
-        return res
+        return res.single()[0]
 
-    def get_node(self, node_name):
-        res = self.__graph.nodes.match("Atc", name=node_name).first()
-        return res
-       # raise HTTPException(status_code=500, detail=f"Node {node_name} not found.")
+    @classmethod
+    def add_atc(cls, tx, name):
+        tx.run("MERGE (a:Atc {name: $name})"
+               "ON CREATE SET a.createdAt = timestamp()", name=name)
+
+    @classmethod
+    def create_interaction(cls, tx, name_a, name_b, level):
+        tx.run("MATCH (a:Atc {name: $name_a}) "
+               "MATCH (b:Atc {name: $name_b}) "
+               "MERGE (a)-[r1:INTERACTS {level: $level}]->(b)"
+               "MERGE (a)<-[r2:INTERACTS {level: $level}]-(b)",
+               name_a=name_a, name_b=name_b, level=level)
+
+    @classmethod
+    def select_interaction(cls, tx, name_a, name_b):
+        res = tx.run("MATCH (a:Atc {name: $name_a}) "
+                     "-[r:INTERACTS]- "
+                     "(b:Atc {name: $name_b})"
+                     "RETURN r.level AS RelationshipLevel",
+                     name_a=name_a, name_b=name_b)
+        if res.peek() is None:
+            return None
+        return res.single()[0]
+
+
+if __name__ == "__main__":
+    greeter = GraphHandler("bolt://localhost:7687", "neo4j", "password")
+    # greeter.print_greeting("hello, world")
+    res = greeter.read_relation("Ozan", "NS")
+    print(res)
+    greeter.close()
---
+++ handlers/graph_handler_deprecated.py
@@ -0,0 +1,38 @@
+from fastapi import HTTPException
+from py2neo import Graph, Node, Relationship
+
+
+class GraphHandler:
+    def __init__(self, uri: str, auth: tuple):
+        self.__graph = Graph(f"{uri}")
+
+    def add_new_relation(self, node_1_lbl: str, node_2_lbl: str, level: list):
+        node_1 = self.get_node(node_1_lbl)
+        node_2 = self.get_node(node_2_lbl)
+        if node_1 is None:
+            node_1 = Node("Atc", name=node_1_lbl)
+        if node_2 is None:
+            node_2 = Node("Atc", name=node_2_lbl)
+        try:
+            self.__graph.create(Relationship(node_1, "INTERACTS", node_2, level=level[0]["interactionLevel"]))
+            self.__graph.create(Relationship(node_2, "INTERACTS", node_1, level=level[0]["interactionLevel"]))
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+
+    def get_relationship(self, atc_1, atc_2):
+        node_1 = self.get_node(atc_1)
+        node_2 = self.get_node(atc_2)
+        if node_1 is None or node_2 is None:
+            return None
+        res = self.__graph.match((node_1, node_2)).first()
+        return res
+
+    def get_node(self, node_name):
+        res = self.__graph.nodes.match("Atc", name=node_name).first()
+        return res
+    # raise HTTPException(status_code=500, detail=f"Node {node_name} not found.")
+
+
+gh = GraphHandler("neo4j://localhost:7474", ("neo4j", "123456"))
+gh.add_new_relation("zaa", "yaa", [{"interactionLevel": "SEVERE"}])
+print(gh.get_node("zaa"))
