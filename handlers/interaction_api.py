a1cfdfc76f066dcd2c06527ce7ee83957c54b0d6
UlviDemirsoy <tedulvi@gmail.com>
2024-05-21 21:49:45 +0300

    neo4j

--- handlers/interaction_api.py
+++ handlers/interaction_api.py
@@ -1,16 +1,14 @@
 import requests
+import aiohttp
+import asyncio
 
 
-class InteractionAPI():
+class InteractionAPI:
     def __init__(self, ip, port):
         self.__url = f"http://{ip}:{port}/api/Interactions/CheckInteractions"
 
     def get_interactions(self, drug_name, selected_drug):
-        if drug_name == "Ketorolac":
-            return "SEVERE"
-        else:
-            return None
-        # body = {"ingredient1": drug_name, "ingredient2": selected_drug}
-        # res = requests.post(self.__url, json=body)
-        # return res.json()
+        body = {"ingredient1": drug_name, "ingredient2": selected_drug}
+        res = requests.post(self.__url, json=body)
+        return res.json()
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
@@ -17,7 +18,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 es_agg = EsAggregator(conf["ES_INDEX"], conf["ES_PORT"])
 interaction_api = InteractionAPI(conf["INTERACTION_API_IP"], conf["INTERACTION_API_PORT"])
-graph_handler = GraphHandler(conf["NEO4J_URI"], (conf["NEO4J_USER"], conf["NEO4J_PSW"]))
+graph_handler = GraphHandler(conf["NEO4J_URI"], conf["NEO4J_USER"], conf["NEO4J_PSW"])
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -45,17 +46,42 @@ async def search_endpoint(data: str = Form(...), stored: str = Form(...)):
         interaction = graph_handler.read_relation(val, stored_atc)
         if interaction is not None:
             interaction_list.append({"drug": key, "atc": val, "interaction_level": [
-                  {
+                {
                     "ingredient1": val,
                     "ingredient2": stored_atc,
-                    "interactionLevel": interaction["level"]
-                  }
-                ]})
+                    "interactionLevel": interaction
+                }
+            ]})
         else:
             interaction = interaction_api.get_interactions(val, stored_atc)
-            interaction_list.append({"drug": key, "atc": val, "interaction_level": interaction})
-            # TODO: Check if interaction level is parsed correctly
-            graph_handler.write_relation(val, stored_atc, interaction)
+            if interaction:
+                interaction_list.append({"drug": key, "atc": val, "interaction_level": interaction})
+                # TODO: Check if interaction level is parsed correctly
+                graph_handler.write_relation(val, stored_atc, interaction[0]['interactionLevel'])
+            else:
+                interaction_list.append({"drug": key, "atc": val, "interaction_level":[
+                {
+                    "ingredient1": val,
+                    "ingredient2": stored_atc,
+                    "interactionLevel": "NO INTERACTION"
+                }]})
+    return interaction_list
+
+
+@app.post("/recursive_autocomplete2", response_class=JSONResponse)
+async def search_endpoint2(data: str = Form(...), stored: str = Form(...)):
+    results = es_agg.get_autocomplete_results(data)
+    autocomplete_list = {}
+    for res in results["aggregations"]["auto_complete"]["buckets"]:
+        atc_name = es_agg.get_atc_from_brand(res["key"])
+        autocomplete_list[res["key"]] = atc_name
+
+    interaction_list = []
+    stored_atc = es_agg.get_atc_from_brand(stored)
+    for key, val in autocomplete_list.items():
+        interaction = interaction_api.get_interactions(val, stored_atc)
+        interaction_list.append({"drug": key, "atc": val, "interaction_level": interaction})
+        # TODO: Check if interaction level is parsed correctly
     return interaction_list
 
 
